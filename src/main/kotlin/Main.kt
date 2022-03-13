d413d61889c20c90bd939f1bef4bdb60d41d2346
Анна Рудницкая <anjarudnitckaia@gmail.com>
2022-03-13 17:16:10 +0300

    Something second

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,6 @@
 fun main(args: Array<String>) {
     println("Hello World!")
-    println("Hello World and me!")
+    println("Hello World and someone else!")
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
